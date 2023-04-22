c308ef4e065de8909e89d9b4045e6c342bc8e571
mohammad-pourbafrani <m.pourbafarani@gmail.com>
2023-04-22 17:10:34 +0430

    init commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:login/presentation/views/utils/login_ui.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Container(),
+    );
+  }
+}
---
+++ lib/presentation/views/utils/login_ui.dart
@@ -0,0 +1,5 @@
+export 'package:flutter_gen/gen_l10n/app_localizations.dart';
+export 'package:login/presentation/views/utils/app_color.dart';
+export 'package:login/presentation/views/utils/app_values.dart';
+export 'package:login/presentation/views/utils/text_style.dart';
+export 'package:login/presentation/views/utils/theme.dart';
